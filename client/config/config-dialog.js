05f5e854b983b164a8c553475cfa4ceec470ad2d
Mateus Barni <mateus.william@ntconsultcorp.com>
2023-04-17 16:08:16 -0300

    change webpack config

---
+++ client/config/config-dialog.js
@@ -0,0 +1,72 @@
+import React, {
+  Fragment,
+  PureComponent
+} from 'camunda-modeler-plugin-helpers/react';
+
+import {
+  Fill
+} from 'camunda-modeler-plugin-helpers/components';
+
+import classNames from 'classnames';
+
+import ConfigDialogOverlay from './config-dialog-overlay';
+
+import OpenIcon from '../../resources/file-excel.svg';
+
+const defaultState = {
+  configOpen: false
+};
+
+export default class ConfigDialogPlugin extends PureComponent {
+
+  constructor(props) {
+    super(props);
+
+    this.state = defaultState;
+    this._buttonRef = React.createRef();
+  }
+
+  handleConfigClosed(closeDetails) {
+    this.setState({ modalOpen: false });
+
+    if (!closeDetails) {
+      return;
+    }
+
+    this.props.config.set('ndgroupCamundaPluginEndpoints', closeDetails.url);
+  }
+
+  openModal() {
+    this.setState({ modalOpen: true });
+  }
+
+  render() {
+    const urlPromise = this.props.config.get('ndgroupCamundaPluginEndpoints', '');
+
+    const initValues = {
+      urlPromise
+    };
+
+    return <Fragment>
+      <Fill slot="tab-actions" group="xx_ndgroup">
+        <button
+          ref={ this._buttonRef }
+          title="Open excel sheet"
+          className={ classNames('btn btn--tab-action', { 'btn--active': this.state.modalOpen }) }
+          onClick={ this.openModal.bind(this) }
+        >
+          <OpenIcon />
+        </button>
+      </Fill>
+
+      {this.state.modalOpen && (
+        <ConfigDialogOverlay
+          anchor={ this._buttonRef.current }
+          onClose={ this.handleConfigClosed.bind(this) }
+          initValues={ initValues }
+        />
+      )}
+    </Fragment>;
+  }
+}
+
